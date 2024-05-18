c5a5749f12df717a2b21f764cb513d3b2aa2925e
Arati somwanshi <7arati10@gmail.com>
2024-05-18 07:30:49 +0530

    Update NavigationBar.jsx

--- dorm/src/Components/NavigationBar.jsx
+++ dorm/src/Components/NavigationBar.jsx
@@ -2,7 +2,7 @@
 import { LinkContainer } from "react-router-bootstrap";
 import { BASE_ROUTE, REGISTRATION_ROUTE, USER_LIST_ROUTE } from "../constants/AppRoute";
 
-import { Button, Col, Container, Form, Row ,Nav , Navbar} from "react-bootstrap";
+import {  Container,Nav , Navbar} from "react-bootstrap";
 export function NavigationBar() {
     return (
         <Navbar expand="lg" bg="dark" data-bs-theme="dark">
@@ -25,22 +25,7 @@ export function NavigationBar() {
                         
 
 
-                        <Form inline>
-              <Row>
-                <Col xs="auto">
-                  <Form.Control
-                    type="text"
-                    placeholder="Search"
-                    className=" mr-sm-2 "
-                    
-                    style={{ width: '380px', height: '40px' ,marginLeft: '80px' }}
-                  />
-                </Col>
-                <Col xs="auto">
-                  <Button type="submit" style={{ width: '70px', height: '40px' }} >Submit</Button>
-                </Col>
-              </Row>
-            </Form>
+             
 
                         
 
