ad831f23825f462a9addc852ac9da4ff989e6e33
AratiSomwanshi <7arati10@gmail.com>
2024-05-20 09:47:18 +0530

    Create DormBooking js and css

--- dorm/src/App.js
+++ dorm/src/App.js
@@ -1,22 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { NavigationBar } from "./Components/NavigationBar";
-import { Home } from "./Components/Home";
-import { Registration } from "./Components/Registrastion";
-import { UserList } from "./Components/UserList";
-import { BASE_ROUTE, REGISTRATION_ROUTE, USER_LIST_ROUTE ,DORM_SEARCH} from "./constants/AppRoute";
-import Footer from "./Components/Footer";
-import {DormSearch} from "./Components/DormSearch";
+import { NavigationBar } from "./components/NavigationBar";
+import { Home } from "./components/Home";
+import { Registration } from "./components/Registrastion";
+
+import { BASE_ROUTE, REGISTRATION_ROUTE, DORM_LIST_ROUTE ,DORM_SEARCH} from "./constants/AppRoute";
+import DormBooking from './components/DormBooking';
+
+
+
+import { DormSearch } from './components/DormSearch';
+
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <NavigationBar/>
-      <Footer/>
+  
       <Routes>
         <Route path={BASE_ROUTE} element={<Home/>} />
         <Route path={REGISTRATION_ROUTE} element={<Registration/>} />
-        <Route path={USER_LIST_ROUTE} element={<UserList/>} />
+        <Route path={DORM_LIST_ROUTE} element={<DormBooking/>} />
         <Route path={DORM_SEARCH} element={<DormSearch/>}/>
 
       </Routes>
--- dorm/src/constants/AppRoute.js
+++ dorm/src/constants/AppRoute.js
@@ -1,4 +1,4 @@
 export const BASE_ROUTE="/";
 export const REGISTRATION_ROUTE="/register-user";
-export const USER_LIST_ROUTE="/user-list";
+export const DORM_LIST_ROUTE="/dorm-list";
 export const DORM_SEARCH="/dormsearch";
